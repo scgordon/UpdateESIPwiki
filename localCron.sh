7eba21fbb839a715ae0ccf7fccc2bd44ac9b9034
scgordon <scgordon@hdfgroup.org>
2016-11-01 17:14:37 -0600

    improvements
    increase dialect glossaries improve recommendation pages, finalize cron
    job to run the update each morning.

---
+++ localCron.sh
@@ -0,0 +1,6 @@
+/Users/scgordon/UpdateESIPwiki/writeWikiSpiralTableFiles.sh &&
+/Users/scgordon/UpdateESIPwiki/writeAllDialectsGlossaries.sh &&
+/Users/scgordon/UpdateESIPwiki/writeWikiGlossary.sh &&
+/Users/scgordon/UpdateESIPwiki/updateWikiRecSpiral.sh &&
+/Users/scgordon/UpdateESIPwiki/updateWikiGlossary.sh 
+#../AllCrosswalksTests/TheDailyCrosswalk.sh
